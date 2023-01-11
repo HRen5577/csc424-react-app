c83ac6cab800cbb785c18ac63940e46f698d3e1d
hrend <hrendon@calpoly.edu>
2023-01-11 14:01:04 -0800

    a1_s3_cpt

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,6 +1,27 @@
+import { Routes, Route, Link } from 'react-router-dom';
+import Landing from './Landing'
+import Home from './Home';
 
 const App = () => {
-    return <></>
-}
+    return( 
+    <>
+        <Navigation/>
+        <h1>React Router</h1>
+        <Routes>
+            <Route index element={<Landing/>}/>
+            <Route path="landing" element={<Landing/>}/>
+            <Route path="home" element={<Home/>}/>
+            <Route path="*" element={<p>There's nothing here: 404</p>}/>
+        </Routes>
+    </>
+    );
+};
+
+const Navigation = () =>(
+    <nav>
+        <Link to="/landing">Landing</Link>
+        <Link to="/home">Home</Link>
+    </nav>
+);
 
 export default App;
---
+++ react-frontend/src/Landing.js
@@ -0,0 +1,5 @@
+const Landing = () =>{
+    return <h2>Landing (Public: anyone can access this page)</h2>;
+};
+
+export default Landing;
